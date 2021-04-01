7e9321f5fd06bf39e4e4e836b35ac0d5a657034e
syegournov <syegournov@outbrain.com>
2021-04-01 16:28:18 +0500

    add workload param to test frozen map

--- src/main/kotlin/com/thelastpickle/tlpstress/profiles/Maps.kt
+++ src/main/kotlin/com/thelastpickle/tlpstress/profiles/Maps.kt
@@ -20,25 +20,31 @@ class Maps : IStressProfile {
     @WorkloadParameter("Use frozen type.")
     var frozen = "false"
 
+    @WorkloadParameter("Limit select to N rows.")
+    var ncols = 1
+
     override fun prepare(session: Session) {
-        insert = session.prepare(
+        val update_args = (1..ncols).joinToString(",") { i ->
             if (frozen == "true") {
-                "UPDATE map_stress SET data = ? WHERE id = ?"
-            } else
-            {
-                "UPDATE map_stress SET data[?] = ? WHERE id = ?"
+                "data$i = ?"
+            } else {
+                "data$i[?] = ?"
             }
-        )
+        }
+        insert = session.prepare("UPDATE map_stress SET $update_args WHERE id = ?")
         select = session.prepare("SELECT * from map_stress WHERE id = ?")
         delete = session.prepare("DELETE from map_stress WHERE id = ?")
     }
 
     override fun schema(): List<String> {
-        val query = if (frozen == "true") {
-            """ CREATE TABLE IF NOT EXISTS map_stress (id text, data frozen<map<text, text>>, primary key (id)) """}
-        else {
-            """ CREATE TABLE IF NOT EXISTS map_stress (id text, data map<text, text>, primary key (id)) """
+        val cols = (1..ncols).joinToString(", ") { i ->
+            if (frozen == "true") {
+                "data$i frozen<map<text, text>>"
+            } else {
+                "data$i map<text, text>"
+            }
         }
+        val query =  """ CREATE TABLE IF NOT EXISTS map_stress (id text, $cols, primary key (id)) """
         return listOf(query)
     }
 
@@ -46,11 +52,15 @@ class Maps : IStressProfile {
     override fun getRunner(context: StressContext): IStressRunner {
         return object : IStressRunner {
             override fun getNextMutation(partitionKey: PartitionKey): Operation {
+                val map = mapOf("key" to "value")
+                val bound = insert.bind()
                 return if (frozen == "true") {
-                    val map = mapOf("key" to "value")
-                    Operation.Mutation(insert.bind(map, partitionKey.getText()))
+                    (1..ncols).forEach(){i -> bound.setMap("data$i", map)}
+                    Operation.Mutation(bound.setString("id", partitionKey.getText()))
                 } else {
-                    Operation.Mutation(insert.bind("key", "value", partitionKey.getText()))
+                    (1..ncols).forEach(){i -> bound.setString((i-1)*2, "key")}
+                    (1..ncols).forEach(){i -> bound.setString((i-1)*2+1, "value")}
+                    Operation.Mutation(bound.setString("id", partitionKey.getText()))
                 }
             }
 
